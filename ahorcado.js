ab4380f2678dfb03356f097b4b6b5362fd722a7c
Marilina Maniscalco <marilinam@gmail.com>
2024-09-26 14:56:29 -0300

    cambios realizados

--- ahorcado.js
+++ ahorcado.js
@@ -1,24 +1,24 @@
 // ### VARIABLES ###
 
 
-var palabras = [["Api", "Iterfasz entre usuario y servidor"],
- ["Desarollo", "Diseñador de paginas mobili"],
+var palabras = [["Api", "Interfaz entre usuario y servidor"],
+ ["Desarollo", "Diseñador de paginas mobiles"],
  ["github", "Herramienta para crear una branch"],
   ["Facebook", "Red Social mas conocida"], 
  ["html", "Esqueleto de una pagina web"], 
  ["Algoritmos", "Series de pasos que se ejecutan para resolver un tipo de problema"], 
- ["petanca", "Un juego"], ["higuera", "Un árbol"],
-  ["Notepac", "Editor de texto y código libri"], 
+ ["MonkeyIsland", "Un juego"], ["higuera", "Un árbol"],
+  ["Notepad", "Editor de texto y código libri"], 
  ["Servidor", "Un programa informático que procesa una aplicación  "],
   ["jirafa", "Un animal"], 
  ["luxemburgo", "Un país"], ["uruguay", "Un país"], 
  ["ilustracion", "Representación gráfica"],
   ["excursion", "Actividad en la naturaleza"], 
- ["empanadilla", "De la panadería"], 
+ ["bizcocho", "De la panadería"], 
  ["pastel", "De la pastelería"],
   ["colegio", "Lugar para estudiar"],
    ["carrera", "Competición"], 
-  ["pitbull", "Insectos"]];
+  ["hormigas", "Insectos"]];
 
 var palabra = "";
 
